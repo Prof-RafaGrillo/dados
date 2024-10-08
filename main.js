777853346f0a79098d4baf738067e5e5e6c9c0a8
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2024-10-08 07:50:51 -0300

    .

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ async function visualizarDados(){
     const res = await fetch(url)
     const dados = await res.json()
     regiao = 0
-     resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+    resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
-    console.log(resultado_dados, resultado_regiao)
+    
   
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos--container__texto')
