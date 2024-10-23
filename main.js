84b9cf0f2f975a699ebbd4cb5655c7a11cc5ff68
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2024-10-23 09:18:05 -0300

    .

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ async function visualizarDados(){
     regiao = 0
     resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
     resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
-    
-    console.log(resultado_dados)
+    let ano = Object.keys(resultado_dados)
+    console.log(ano[0])
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos--container__texto')
-    paragrafo.innerHTML =`${resultado_dados}`
+    paragrafo.innerHTML =`${ano[0]}`
  
     const container = document.getElementById('graficos--container')
 
